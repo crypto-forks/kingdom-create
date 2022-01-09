d16f316e54506bf5ece6be5bc326ed8160123941
Potato Potato <potatopotato@Potatos-MacBook-Air.local>
2022-01-09 20:42:52 +0200

    Initial.

---
+++ README.md
@@ -0,0 +1,37 @@
+# 🦄 create-buildspace-dao
+
+`create-buildspace-dao` generates DAO starter boilerplate made by Buildspace community. 
+
+
+## Getting started
+
+```
+npx create-buildspace-dao my-dao
+```
+
+Copy the `.env.example` file as `.env` and provide values for all required fields.
+
+Start the application in development mode:
+```
+cd my-dao && npm run start
+```
+
+## Development
+
+Fallow [Buildspace: Build your own DAO with just Javascript in a weekend](https://app.buildspace.so/projects/COb520aae3-7925-42f4-a5e7-eaf718933766) project tutorial.
+
+> Seperate open-sourced documentation is under construction.
+
+## Deployment
+
+Generate the production build of the application:
+```
+npm run build
+```
+
+Using the web server of your preference, publish the contents of the generated `build` directory.
+
+## Resources
+- [Buildspace: Build your own DAO with just Javascript in a weekend](https://app.buildspace.so/projects/COb520aae3-7925-42f4-a5e7-eaf718933766)
+- [Thirdweb](https://thirdweb.com/)
+- [Alchemy](https://www.alchemy.com/)
---
+++ bin/clone.js
@@ -0,0 +1,50 @@
+#! /usr/bin/env node
+
+const { execSync } = require('child_process');
+const path = require('path');
+const fs = require('fs');
+
+const GIT_REPO = 'https://github.com/buildspace/buildspace-dao-final.git';
+
+if (process.argv.length < 3) {
+    console.log('ℹ️  You have to provide a name to your dao.');
+    console.log('e.g.: npx create-buildspace-dao my-dao');
+
+    process.exit(1);
+}
+
+const projectName = process.argv[2];
+const currentPath = process.cwd();
+const projectPath = path.join(currentPath, projectName);
+
+try {
+    fs.mkdirSync(projectPath);
+} catch (error) {
+    if (error.code === 'EEXIST') {
+        console.log(`🔴 The file ${projectName} already exist in the current directory, please give it another name.`);
+    } else {
+        console.error(`🔴 ${error}`);
+    }
+    process.exit(1);
+}
+
+
+async function clone() {
+    try {
+        console.log('⌛ Downloading files ...');
+        execSync(`git clone --depth 1 ${GIT_REPO} ${projectPath}`);
+
+        process.chdir(projectPath);
+
+        console.log('⌛ Installing dependencies ...');
+        execSync('npm install --loglevel=error');
+        execSync('npx rimraf ./.git');
+
+        console.log('✅ Ready to use!');
+
+    } catch (error) {
+        console.error(`🔴 ${error}`);
+    }
+}
+
+clone();
